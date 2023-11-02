cb2421f865f56d2ced40937ba2be07809c61e981
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2023-11-02 23:23:50 +0530

    finished

--- app/src/main/java/com/rush/appstorage/MainActivity.java
+++ app/src/main/java/com/rush/appstorage/MainActivity.java
@@ -10,6 +10,9 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("CommitPrefEdits")
@@ -25,22 +28,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-            //Renames the XML file to the activity name
+
+//        //Option 1
+//        //Renames the XML file to the activity name
 //        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
 //        SharedPreferences.Editor edit = preferences.edit();
 //        edit.putString("name", "Java");
 //        edit.apply();
 //
-        // Can give a name to the XML file
-//        SharedPreferences preferences = getSharedPreferences("data",Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = preferences.edit();
+//        //Option 2
+//        // Can give a name to the XML file
+//        SharedPreferences preferences2 = getSharedPreferences("data",Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = preferences2.edit();
 //        editor.putString("name","APP DATA FORM SHARED PREFERENCES");
 //        editor.apply();
+//
+//        //Option 3
+//        //Renames the XMP file to package name
+//        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        SharedPreferences.Editor editor2 = sharedPreferences.edit();
+//        editor2.putString("name3","App data store from 3");
+//        editor2.apply();
 
-        //Renames the XMP file to package name
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("name3","App data store from 3");
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        SharedPreferences.Editor editor = preferences.edit();
+
+        Set<String> data = new HashSet<>();
+
+        data.add("A");
+        data.add("B");
+        data.add("C");
+        data.add("D");
+
+        editor.putStringSet("names", data);
         editor.apply();
+
+        Set<String> names = preferences.getStringSet("names", null);
+
+
     }
 }
